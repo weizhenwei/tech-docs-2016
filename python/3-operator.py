1826fd251892c779d1637f2ee94c9365c79aa74e
weizhenwei <weizhenwei1988@gmail.com>
2016-08-04 23:59:59 +0800

    add python/3-operator.py

---
+++ python/3-operator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+a = 21
+b = 32
+c = a + b
+print c
+
+if (a == b):
+    print "a == b"
+else:
+    print "a != b"
+
+list = [1, 2, 3, 4, 5];
+print list
+if (a in list):
+    print "21 in list"
+else:
+    print "21 not in list"
+
+
