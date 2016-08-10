d3e38ba3fc46ca2bffb0a1ab548381f5b5baf0e7
weizhenwei <weizhenwei0114@qq.com>
2016-08-10 23:43:34 +0800

    add python/3-if.py

---
+++ python/3-if.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+a = 21
+b = 3
+
+c= a + b
+print "c = ", c
+
+
+
+
+flas = False
+name = "Eric"
+
+if name == "python":
+    print "Welcome, python"
+else:
+    print "Welcome, ", name
+
+
+name = "javascript"
+if name == "python":
+    print "Welcome, python"
+elif name == "javascript":
+    print "Welcome, javascript"
+else:
+    print "Welcome, ", name
+
+
+
+num = 9
+if num >= 0 and num <= 10:
+    print " 0 <= num <= 10"
+
