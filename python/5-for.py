1be4972ca39408b8d4770b5722642996908c9a70
weizhenwei <weizhenwei0114@qq.com>
2016-08-17 23:45:45 +0800

    add 5-for.py

---
+++ python/5-for.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+for letter in 'python':
+    print "Current character is ", letter
+
+
+fruits = ['banana', 'apple', 'mango']
+for fruit in fruits:
+    print "Current fruit is ", fruit
+
+for index in range(len(fruits)):
+    print "Current fruit is ", fruits[index]
+
+
+print "Good bye!"
+
