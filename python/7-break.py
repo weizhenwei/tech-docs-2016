b330825fcdca52a04660eac30afd6cfcccaa5c56
weizhenwei <weizhenwei0114@qq.com>
2016-08-19 23:45:23 +0800

    add 7-break.py

---
+++ python/7-break.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+for letter in "Python":
+    if letter == 'h':
+        break
+    print "Current charater is ", letter
+
+var = 10
+while var > 0:
+    print "Current charater is ", var
+    var = var - 1
+    if var == 5:
+        break
+
+
+print "Good bye!"
+
