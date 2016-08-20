e19139f093ccd393e05c3fb490477c9537504620
weizhenwei <weizhenwei0114@qq.com>
2016-08-20 23:45:01 +0800

    add 8-continue.py

---
+++ python/8-continue.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+for letter in "Python":
+    if letter == 'h':
+        continue
+    print "Current charater is ", letter
+
+var = 10
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+    print "Current charater is ", var
+
+print "Good bye!"
+
