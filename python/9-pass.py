4a99ea57f080ded0ce1f9ba21e41303f9dd07572
weizhenwei <weizhenwei0114@qq.com>
2016-08-21 23:45:23 +0800

    add 9-pass.py

---
+++ python/9-pass.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+for letter in "Python":
+    if letter == 'h':
+        pass
+        print "this is pass block"
+    print "Current charater is ", letter
+
+var = 10
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+    print "Current charater is ", var
+
+print "Good bye!"
+
