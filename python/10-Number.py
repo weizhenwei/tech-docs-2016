e22ed8f2c8d0dcc7e22d33fe94eae1799202c582
weizhenwei <weizhenwei0114@qq.com>
2016-08-23 23:45:12 +0800

    add 10-Number.py

---
+++ python/10-Number.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import math
+
+var1 = 1
+var2 = 10
+
+print "var1 = ", var1
+print "var2 = ", var2
+
+del var1
+
+# after del, var1 is not defined
+# print "var1 is not defined"
+print "var2 = ", var2
+
+varpi = math.pi
+vare = math.e
+print "varpi = ", varpi
+print "vare = ", vare
+
+
