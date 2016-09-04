20f0fa70008f3dd64386c36aabafaf3c087c455a
weizhenwei <weizhenwei0114@qq.com>
2016-09-04 12:23:34 +0800

    add 17-module/support.py

---
+++ python/17-module/support.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+def printme(str):
+    "print something input"
+    print str
+    return
+
+def printmemore(str):
+    "print something input"
+    print "hehe, ", str
+    return
+
+printmemore("goog")
+
