ee2a4c1edb6d2f1273bb08080e8fc00b0a0e9074
weizhenwei <weizhenwei0114@qq.com>
2016-09-10 23:23:23 +0800

    add pack1/mymodule1.py

---
+++ python/18-package/parent/pack1/mymodule1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding=utf-8 -*-
+
+def function1():
+    print "function1 running"
+
+
+if __name__ == "__main__":
+    print "mymodule1 running as main program"
+else:
+    print "mymodule1 initializing"
+
