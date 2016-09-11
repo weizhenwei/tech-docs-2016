a03fe9693f4d8db0ed3accbc22f895a3c476cc6d
weizhenwei <weizhenwei0114@qq.com>
2016-09-11 21:24:29 +0800

    add 18-package/parent/pack2/mymodule2.py

---
+++ python/18-package/parent/pack2/mymodule2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding=utf-8 -*-
+
+def function2():
+    print "function2 running"
+
+
+if __name__ == "__main__":
+    print "mymodule2 running as main program"
+else:
+    print "mymodule2 initializing"
+
