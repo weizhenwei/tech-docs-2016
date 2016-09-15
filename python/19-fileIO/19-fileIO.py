dab9bc0a4ca0926389257d190bf7551181782023
weizhenwei <weizhenwei0114@qq.com>
2016-09-15 14:56:25 +0800

    add 19-fileIO.py for tell and seek

--- python/19-fileIO/19-fileIO.py
+++ python/19-fileIO/19-fileIO.py
@@ -19,12 +19,15 @@ fileobj.write("Hello, world!\n")
 fileobj.close()
 
 
+# tell
 fileobj = open("a.txt", "r", 0)
-print "filename", fileobj.name
-print "closed", fileobj.closed
-print "access mode", fileobj.mode
-print "softspace", fileobj.softspace
-sstr = fileobj.read();
+sstr = fileobj.read(10)
 print "read file:", sstr
-fileobj.close()
+position = fileobj.tell()
+print "current position:", position
+fileobj.seek(0, 0)
+sstr = fileobj.read(10)
+print "read file again:", sstr
+position = fileobj.tell()
+print "current position again:", position
 
