1bb65d05aaa1bab5425b189acf2b39a21ff98731
weizhenwei <weizhenwei1988@gmail.com>
2017-01-28 23:23:23 +0800

    add programming/extern_c/extern_c.h

---
+++ programming/extern_c/extern_c.h
@@ -0,0 +1,21 @@
+/*
+ *  test extern "C"
+ *  2017.05.09
+ *  weizhenwei
+ */
+
+#ifndef EXTERN_C_H
+#define  EXTERN_C_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void print_hello();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // #ifndef EXTERN_C_H
+
